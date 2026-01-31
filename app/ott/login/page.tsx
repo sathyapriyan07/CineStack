67d05b25ee124f7408221b702edfda344e6c1e88
Sathya Priyan <sathyapriyan710@gmail.com>
2026-01-31 22:43:24 +0530

    Update project

--- app/ott/login/page.tsx
+++ app/ott/login/page.tsx
@@ -1,10 +1,15 @@
+
+"use client";
+import { Suspense } from "react";
 import LoginForm from '@/components/auth/login-form';
 
 export default function LoginPage() {
   return (
     <main className="min-h-[60vh] flex flex-col items-center justify-center p-4">
       <h1 className="text-xl font-bold mb-4 text-white">Login</h1>
-      <LoginForm />
+      <Suspense>
+        <LoginForm />
+      </Suspense>
     </main>
   );
 }
